aa03b806e525afb5951cdee95b282700798661f4
Kris <me@restrose.net>
2016-05-16 10:29:53 +0800

    ok

--- src/Core/WeChatAPI.php
+++ src/Core/WeChatAPI.php
@@ -2,18 +2,20 @@
 
 namespace FooWeChat\Core;
 
-use App;
+use App\Department;
+use App\Member;
+use App\ServerVal;
 use Config;
 use Cookie;
 use Input;
 use Request;
 use Session;
 use GuzzleHttp\Client;
+use Psr\Http\Message\StreamInterface;
 
 /**
 * 微信核心API
 */
-
 class WeChatAPI
 {
 
@@ -24,8 +26,6 @@ class WeChatAPI
 
     protected $client;
     protected $oAuth2UserInfoArray;
-
-    protected $tmp;
     
     public $code;
     
@@ -39,12 +39,12 @@ class WeChatAPI
      */
     public function __construct()
     {
-        $this->corpID = $this->conf('corpID');
+        $this->corpID     = $this->conf('corpID');
         $this->corpSecret = $this->conf('corpSecret');
         $this->corpTalken = $this->conf('token');
-        $this->agentID = $this->conf('agentID');
+        $this->agentID    = $this->conf('agentID');
     }
-   /**
+    /**
      * 获取配置文件信息
      *
      * @param string 
@@ -67,12 +67,22 @@ class WeChatAPI
      */
     private function searchServeVal($key)
     {
-        $recs = App\ServerVal::where('var_name', $key)
-                            ->where('var_up_time', '>', (time() - 7200))
-                            ->get();
-        foreach ($recs as $rec) {
-            return $rec->var_value;
+        $ex = ServerVal::where('var_name', $key)->first();
+        if(count($ex)){
+            $expire = $ex->expire;
+            $rec = ServerVal::where('var_name', $key)
+                            ->where('var_up_time', '>', (time() - $expire))
+                            ->first();
+            if(count($rec)){
+                return $rec->var_value;
+            }else{
+                return $rec;
+            }
+            
+        }else{
+            return $ex;
         }
+
     }
 
    /**
@@ -87,20 +97,23 @@ class WeChatAPI
 	{
 
         $serverVal = $this->searchServeVal('token');
+
 		if(count($serverVal))
         {
             return $serverVal;
         }else{
             
-            $weChatGetTalkenUrl = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=".$this->corpID."&corpsecret=".$this->corpSecret;
+            $wechat_get_token_url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=".$this->corpID."&corpsecret=".$this->corpSecret;
             $client = new Client();
-            $json = $client->get($weChatGetTalkenUrl)->getBody();
+            $json = $client->get($wechat_get_token_url)->getBody();
             $arr = json_decode($json, true);
-            $wehatToken = $arr['access_token'];
+            
+            $wehat_token = $arr['access_token'];
+            $expire = $arr['expires_in'];
 
-            $go = App\ServerVal::updateOrCreate(['var_name' => 'token'], ['var_value'=> $wehatToken, 'var_up_time' => time()]);
+            $go = ServerVal::updateOrCreate(['var_name' => 'token'], ['var_value'=> $wehat_token, 'expire'=>$expire, 'var_up_time' => time()]);
 
-            return $wehatToken;
+            return $wehat_token;
         };
 	}
 
@@ -113,10 +126,10 @@ class WeChatAPI
      */
     public function oAuth2()
     {
-        $redirectUrl = Request::url();
-        $weChatOauth2Url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$this->corpID."&redirect_uri=".$redirectUrl."&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
+        $redirect_url = Request::url();
+        $wechat_Oauth2_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$this->corpID."&redirect_uri=".$redirect_url."&response_type=code&scope=SCOPE&state=STATE#wechat_redirect";
 
-        header("Location: $weChatOauth2Url");
+        header("Location: $wechat_Oauth2_url");
         exit;
     }
     
@@ -129,44 +142,223 @@ class WeChatAPI
      */
     public function oAuth2UserInfo()
     {
-        $weChatOauth2UserInfoUrl = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=".$this->getAccessToken()."&code=".$this->code;
+        $wechat_Oauth2_user_info_url = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=".$this->getAccessToken()."&code=".$this->code;
         $client = new Client();
-        $json = $client->get($weChatOauth2UserInfoUrl)->getBody();
+        $json = $client->get($wechat_Oauth2_user_info_url)->getBody();
         $arr = json_decode($json, true);
         $this->oAuth2UserInfoArray = $arr;
     }
 
+    /**
+     * 设置用户session, cookie
+     *
+     * @param $userid
+     *
+     * @return mix
+     */
     public function weChatUserSetCookieAndSession()
-    {
+    {     
         $arr = $this->oAuth2UserInfoArray;
         $userid = $arr['UserId'];
         $deviceid = $arr['DeviceId'];
 
-        $recs = App\Member::where('work_id', $userid)->get();
+        $rec = Member::where('work_id', $userid)->first();
 
-        if(count($recs)){
-            foreach ($recs as $rec) {
-                if($rec->state === 0){
-                    //账号状态正常
-                    if(!Session::has('id')) Session::put('id', $rec->id);
-                    if(!Session::has('name')) Session::put('name', $rec->name);
-                    if(!Session::has('department')) Session::put('department', $rec->department);
-                    if(!Session::has('position')) Session::put('position', $rec->position); 
+        if(count($rec)){
+            
+            if($rec->state === 0){
+                //账号状态正常
+                if(!Session::has('id')) Session::put('id', $rec->id);
+                if(!Session::has('name')) Session::put('name', $rec->name);
 
-                    Cookie::queue('id', $rec->id, 20160);
+                Cookie::queue('id', $rec->id, 20160);
 
-                }else{
-                    return view('40x')->with('errorCode','3');//权限不足
-                    exit;
-                }
-                
+            }else{
+                return view('40x',['color'=>'warning', 'type'=>'2', 'code'=>'2.1']);
             }
+                
 
         }else{
-            return view('40x')->with('errorCode','4');//未找到用户
-            exit;
+            return view('40x',['color'=>'danger', 'type'=>'1', 'code'=>'1.2']);
         }
 
     }
 
+
+    /**
+     * 初始化部门: 批量建立部门
+     *
+     * 要求: 从未创建部门或者清除所有部门,只余根部门
+     *
+     * @param json
+     *
+     * @return view or redirect
+     */
+    public function initDepartments ()
+    {
+        $recs = Department::where('id', '>', 1)->get();
+        //$arr = array();
+
+        if(count($recs)){
+
+            foreach ($recs as $rec) {
+                $arr  = [
+                            'name'     => $rec->name, 
+                            'parentid' => $rec->parentid, 
+                            'order'    => $rec->order, 
+                            'id'       => $rec->id,
+                        ];
+
+                $this->createDepartment($arr);
+            } 
+
+        }else{
+            return view('40x',['color'=>'red', 'type'=>'1', 'code'=>'1.2']);
+        }
+    }
+
+    /**
+     * 初始化部门: 新建部门
+     *
+     * 待改进: 从其他模块调用,不能等待返回值 !!!
+     *
+     * @param array :['name', 'parentid', 'order', 'id']
+     *
+     * @return null
+     */
+    public function createDepartment ($array)
+    {
+        $post_JSON = json_encode($array, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+        $wechat_department_create_url = 'https://qyapi.weixin.qq.com/cgi-bin/department/create?access_token='.$this->getAccessToken();
+
+        $client = new Client();
+        $client->request('POST', $wechat_department_create_url, ['body' => $post_JSON])->getBody();
+        
+        // $err = json_decode($rs, true);
+
+        // if($err['errcode'] != 0){
+        //     return view('40x',['errorCode' => '5', 'msg' => $err['errmsg']]); // 5: 微信服务器错误
+        //     exit;
+        // }
+    }
+
+    public function initUsers ()
+    {
+       $recs = Member::where('members.id', '>', 1)
+                       ->leftJoin('positions', 'members.position', '=', 'positions.id')
+                       ->select('members.*', 'positions.name as positionName')
+                       ->get();
+       //$rec = App\Member::find(2);
+
+        if(count($recs)){
+
+            foreach ($recs as $rec) {
+                $arr  = [
+                            'userid'     => $rec->work_id,
+                            'name'       => $rec->name,
+                            'department' => explode('-',$rec->department),
+                            'position'   => $rec->positionName,
+                            'mobile'     => $rec->mobile,
+                            'gender'     => $rec->gender,
+                            'email'      => $rec->email,
+                            'weixinid'   => $rec->weixinid,
+                        ];
+
+                $this->createUser($arr);
+               
+            } 
+
+        }else{
+            return view('40x',['color'=>'red', 'type'=>'1', 'code'=>'1.2']);
+        } 
+    }
+
+    /**
+     * 新建: 用户
+     *
+     * 待改进: 从其他模块调用,不能等待返回值!!!
+     *
+     * @param array :['userid', 'name', 'departmnet', 'position', 'mobile', 'gender', 'email', 'wexinid']
+     *
+     * @return null
+     */
+    public function createUser ($array)
+    {
+            $post_JSON = json_encode($array, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+
+            $wechat_user_create_url = 'https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token='.$this->getAccessToken();
+
+            $client = new Client();
+            $rs = $client->request('POST', $wechat_user_create_url, ['body' => $post_JSON])->getBody();
+            //$err = json_decode($rs, true);
+            //print_r($err);
+            // $t = $err['errcode'];
+            // if($t > 0){
+            //     return view('40x',['errorCode' => '5', 'msg' => $err['errmsg']]); // 5: 微信服务器错误
+            //     exit;
+            // }
+    }
+
+    /**
+     * 更新: 用户
+     *
+     * 待改进: 从其他模块调用,不能等待返回值!!!
+     *
+     * @param array :['userid', 'name', 'departmnet', 'position', 'mobile', 'gender', 'email', 'wexinid']
+     *
+     * @return null
+     */
+    public function updateUser ($array)
+    {
+            $post_JSON = json_encode($array, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+
+            $wechat_user_update_url = 'https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token='.$this->getAccessToken();
+
+            $client = new Client();
+            $rs = $client->request('POST', $wechat_user_update_url, ['body' => $post_JSON])->getBody();
+            //$err = json_decode($rs, true);
+            //print_r($err);
+            // $t = $err['errcode'];
+            // if($t > 0){
+            //     return view('40x',['errorCode' => '5', 'msg' => $err['errmsg']]); // 5: 微信服务器错误
+            //     exit;
+            // }
+    }
+
+    /**
+    * 删除: 用户
+    * 
+    * @param UserId = members.work_id
+    *
+    * @return JSON
+    */
+    public function deleteUser($work_id)
+    {
+        $wechat_user_delete_url = 'https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token='.$this->getAccessToken().'&userid='.$work_id;
+        $client = new Client();
+        $client->get($wechat_user_delete_url);
+        //$arr = json_decode($json, true);
+
+        //return $arr['errcode'] === 0 ? true : false;
+
+    }
+
+    /**
+    * other functions
+    *
+    */
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -1,8 +1,15 @@
 <?php 
 namespace FooWeChat\Helpers;
 
+use App\Department;
+use App\FormConfig;
+use App\Member;
+use App\Position;
 use Config;
-//use Carbon\Carbon;
+use DB;
+use Session;
+use GuzzleHttp\Client;
+use Psr\Http\Message\StreamInterface;
 
 /**
 * 杂项工具
@@ -31,14 +38,212 @@ class Helper
 		}
 	}
 
-	public function errorCode($key)
+	//错误信息
+	public function errorCode($type, $code)
 	{
-		$errorCodes = [
-			'1' => "禁止访问: 权限不足",
-			'3' => "禁止访问: 您的账号状态可能被锁定",
-			'4' => "错误: 找不到到相关记录",
-		];
+		$errorTypes = [
+						'1' =>'错误',
+						'2' =>'禁止访问',
+						'3' =>'权限不足',
+						'4' =>'重要提示:删除',
+						'5' =>'操作成功',
+		             ];
 
-		return $errorCodes[$key];
+		$errorCodes = [
+						'1.1' => "未知错误",
+						'1.2' => "找不到到相关记录",
+						'1.3' => "密码错误",
+						'1.4' => "微信服务器错误",
+						'1.5' => "必须登录",
+						'2.1' => "您的账户被锁定. 若需要使用本系统, 请联系系统管理员",
+						'2.2' => "密码错误",
+						'2.3' => "此页面必须使用微信访问, 禁止使用其他浏览器",
+						'2.4' => "此页面需要使用常规浏览器访问, 禁止使用微信客户端",
+						'2.5' => "您没有权限继续操作, 可能是您的账号被锁定或删除",
+						'3.1' => "您没有权限进行此项操作, 或者使用不允许的方法访问.若需要继续使用或此次警示属于异常情况, 请联系系统管理员.",
+						'4.1' => "您正在进行 [删除用户] 操作, 此操作将从系统中删除用户记录, 该用户将不能使用本系统, 包括通过微信. 删除是不可恢复的!!",
+						'5.1' => "您的操作已成功!"
+					  ];
+		$arr = [];
+		$arr[] = $errorTypes[$type];
+		$arr[] = $errorCodes[$code];
+
+		return $arr;
 	}
+
+	/**
+	* 获取下拉列表数组
+	*
+	* @param string
+	* @return array 
+	*/
+	public function getSelect($key)
+	{
+		$recs = FormConfig::where('list',$key)->get();
+
+
+        if(count($recs)){
+
+        	$arr =[];
+
+        	foreach ($recs as $rec) {
+        		$arr = array_add($arr, $rec->id, $rec->name);
+        	}
+
+        	return $arr;
+
+        }else{
+        	return  ['0' => 'null', ];
+        }
+     
+	}
+
+	/**
+	* 所有职位
+	*/
+	public function getAllPositions()
+	{
+		$recs = Position::where('id', '>', 1)->orderBy('order','DESC')->get();
+		if(count($recs)){
+			$arr = [];
+			foreach ($recs as $rec) {
+				$arr = array_add($arr, $rec->id, $rec->name);
+			}
+			return $arr;
+		}else{
+			return view('40x',['color'=>'danger', 'type'=>'1', 'code'=>'1.2']);
+		}
+	}
+
+	/**
+	* 2. 所有部门:除了root
+	*
+	*/
+	public function getAllDepartments()
+	{
+		$recs = Department::where('id', '>', 1)->get();
+
+		if(count($recs)){
+			$arr = [];
+			foreach ($recs as $rec) {
+				$arr = array_add($arr, $rec->id, $rec->name);
+			}
+			return $arr;
+		}else{
+			return view('40x',['color'=>'danger', 'type'=>'1', 'code'=>'1.2']);
+		}
+	}
+
+	/**
+	* 2. 所有公司部门
+	*
+	*/
+	public function getInsideDepartments()
+	{
+		//公司内部记录:code
+		$insideCode = '1-2-3%';
+
+		$recs = Department::where('code','LIKE', $insideCode)->get();
+
+		if(count($recs)){
+			$arr = [];
+			foreach ($recs as $rec) {
+				$arr = array_add($arr, $rec->id, $rec->name);
+			}
+			return $arr;
+		}else{
+			return view('40x',['color'=>'danger', 'type'=>'1', 'code'=>'1.2']);
+		}
+	}
+
+	/**
+	* 分配工号
+	*
+	* 不含4, 以部门编号开头, 顺序增
+	*
+	* @param string
+	* @return array 
+	*/
+	public function getWorkId()
+	{
+		$max = Member::max('work_id');
+		if ($max=='') $max = 0;
+		$max++;
+
+		$pos = stripos($max,'4');
+		if ($pos=='') {
+			# code...
+		}else{
+			$length = strlen($max);
+
+			for ($i=($pos+1); $i < $length; $i++) { 
+				$max = substr_replace($max,'0',$i,($length-$pos));
+			}
+
+			$max = str_ireplace("4","5",$max);
+		}
+		
+		$max = intval($max);
+
+		return $max;
+	}
+
+	/**
+	* 检查表记录存在(删除前检查)
+	*
+	* 1.示例: $arr = ['table1'=>'list1|list2', 'table2'=>'list1|list2']
+	*
+	* @param $array, $val
+	*
+	* @return boolean
+	*/
+	public function exsitsIn($array, $val)
+	{
+		foreach ($array as $key => $value) {
+			$keys =  explode('|', $value);
+			foreach ($keys as $k) {
+				$recs = DB::table($key)->where($k, $val)->get();
+				if(count($recs)) return true;
+			}	
+		}
+
+		return false;
+	}
+
+	/**
+	* IP转城市及获取百度地图坐标
+	*
+	* @param IP
+	*
+	* @return array
+	*/
+	public function ipToCity($ip)
+	{
+		$baidu_IP_url = 'http://api.map.baidu.com/location/ip?ak=bsa3LH1GT1jhOep5N7Uz950xtTQWvp9I&ip='.$ip.'&coor=bd09ll';
+		$client = new Client();
+        $json = $client->get($baidu_IP_url)->getBody();
+        
+        return json_decode($json, true);
+	}
+
+	/**
+	* other functions
+	*
+	*/
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
